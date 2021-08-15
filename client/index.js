14d1762965638d732efb0c367cdda22267864690
imthiyasv91351 <imthiyasv81450@gmail.com>
2021-08-16 01:28:39 +0530

    js file and its content added

---
+++ client/index.js
@@ -0,0 +1,17 @@
+const selectEmployee = document.getElementById('employee-select');
+const ipStartDate = document.getElementById('ipStartDate');
+const ipEndDate = document.getElementById('ipEndDate');
+const btnTrack = document.getElementById('btnTrack');
+
+const resultCard = document.getElementById('resultCard');
+const vtName = document.getElementById('vtName');
+const vtHours = document.getElementById('vtHours');
+const btnCheck = document.getElementById('btnCheck');
+
+document.addEventListener('DOMContentLoaded', function () {
+	
+});
+
+btnTrack.onclick = () => {
+
+};
