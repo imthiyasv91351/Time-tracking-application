9d0ea93dce8d9eca33fadceeb03ae47535efbae9
imthiyasv91351 <imthiyasv81450@gmail.com>
2021-08-17 01:22:59 +0530

    updated button action

--- client/index.js
+++ client/index.js
@@ -11,10 +11,29 @@ const btnCheck = document.getElementById('btnCheck');
 document.addEventListener('DOMContentLoaded', function () {
     console.log('loaded');
     fetch('http://localhost:5000/get-employee')
-        .then(response => response.json())
-        .then(data => console.log(data));
+        .then((response) => response.json())
+        .then((data) => {
+            const {employees} = data;
+            let optionMarkup = '<option value="">Select an employee</option>';
+            for (let i = 0; i < employees.length; i++) {
+                const employee = employees[i];
+                optionMarkup += `<option value="${employee.eid}">${employee.eid} - ${employee.name}</option>`;
+            }
+            selectEmployee.innerHTML = optionMarkup;
+        });
 });
 
 btnTrack.onclick = () => {
+    const start = Number(new Date(ipStartDate.value));
+    const end = Number(new Date(ipEndDate.value));
+    const eid = selectEmployee.value;
+    console.log(eid);
 
+    // console.log('start', new Date(start));
+
+    fetch(`http://localhost:5000/time-track?eid=${eid}&start=${start}&end=${end}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
 };
